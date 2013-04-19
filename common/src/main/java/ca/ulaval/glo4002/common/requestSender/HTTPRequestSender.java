9c2105c45eb4cde0ddf43fa86c0e0289e9329c95
Alexandre Girard <girarda.92@gmail.com>
2013-04-19 00:37:06 -0400

    Enlever un print.

--- common/src/main/java/ca/ulaval/glo4002/common/requestSender/HTTPRequestSender.java
+++ common/src/main/java/ca/ulaval/glo4002/common/requestSender/HTTPRequestSender.java
@@ -48,8 +48,6 @@ public class HTTPRequestSender {
     public String sendGETRequest(String resource) {
         WebResource webResource = prepareRequest(resource);
 
-        System.out.println(webResource);
-
         ClientResponse response = webResource.type(APPLICATION_TYPE).get(ClientResponse.class);
 
         treatAnswerFromRequest(response);
